4c2bed66f238fd09ac2041a4e70269318f21b692
Mohammed Gaseer <85013312+gaseer@users.noreply.github.com>
2023-08-19 11:05:49 +0530

    Update colors.dart

--- lib/constants/colors.dart
+++ lib/constants/colors.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 
 class MainColor {
+  
   static Color primaryColor = Color(0xff7829ee);
   static Color secondaryColor = Color(0xfff13acb);
   static Color accentColor = Color(0xfff4fc02);
+  
 }
